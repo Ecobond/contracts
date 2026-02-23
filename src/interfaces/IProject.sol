3aca3d072a0072cd8bbc9219a9de939f5c355eac
David Dada <daveproxy80@gmail.com>
2026-02-23 11:49:12 +0100

    Rename IProjectMod and create ProjectDetails struct

--- src/interfaces/IProject.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IProject {
-    function createProject(string calldata projectURI) external returns (uint256 projectId);
-    function updateProjects(uint256[] calldata projectIds, string[] calldata projectURIs) external;
-}
